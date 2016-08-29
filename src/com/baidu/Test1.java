a9ecf7d492dc38e9647c9b555eab7f05e88f1186
zhaoyanzeng <13651223413@163.com>
2016-08-29 14:33:59 +0800

    0.0.5

--- src/com/baidu/Test1.java
+++ src/com/baidu/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 		
 	public static void main(String[] args) {
 		
-		System.out.println("hello");
+		System.out.println("hellosdafafafafafaf");
 		System.out.println("bbbbbb");
 	}
 }
