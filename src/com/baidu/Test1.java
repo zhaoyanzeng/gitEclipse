43958a48ab49c5fc644c87b901b391f812552989
zhaoyanzeng <13651223413@163.com>
2016-08-29 14:33:03 +0800

    0.0.4

--- src/com/baidu/Test1.java
+++ src/com/baidu/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 		
 	public static void main(String[] args) {
 		
-		System.out.println("hello");
+		System.out.println("helloadadadadadad");
 		System.out.println("又是乱码");
 	}
 }
